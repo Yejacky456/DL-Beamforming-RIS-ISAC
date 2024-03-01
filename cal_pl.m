3182ade035784727e2e9e0ca88e1994ffe78dd47
JJ Ye <56674234+Yejacky456@users.noreply.github.com>
2024-03-01 13:28:31 +0800

    Update cal_pl.m

--- cal_pl.m
+++ cal_pl.m
@@ -1,7 +1,6 @@
 function [hrc_pl,G_pl,hrt_pl] = cal_pl(dist_br,dist_ru,dist_rt)
-% pathloss
-fc = 2.4; % GHz, carrier freq
-n_pw_sp = -76; % Noise power spectral density
+fc = 2.4; 
+n_pw_sp = -76; 
 
 hrc_pl = sqrt(10.^((-n_pw_sp-LOS_PL(dist_ru,fc)) /10));
 hrt_pl = sqrt(10.^((-n_pw_sp-LOS_PL(dist_rt,fc)) /10));
@@ -11,7 +10,3 @@ end
 function [pl] = LOS_PL(d,fc)
 pl = 22*log10(d)+28+20*log10(fc);
 end
-
-% function [pl] = NLOS_PL(d,fc)
-% pl = 36.7*log10(d)+22.7+26*log10(fc);
-% end
