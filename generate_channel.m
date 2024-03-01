d95469fd29c104eceaf4bf2890cbeedfbde8cf5d
JJ Ye <56674234+Yejacky456@users.noreply.github.com>
2024-03-01 09:53:02 +0800

    Update generate_channel.m

--- generate_channel.m
+++ generate_channel.m
@@ -11,7 +11,7 @@ hrt_NLOS = sqrt(1/2/N)*(randn(N,1)+1j*randn(N,1));
 hrt = (sqrt(kappa/(kappa+1))*hrt_LOS+sqrt(1/(kappa+1))*hrt_NLOS) * hrt_pl; % RIS-> UE
 
 G_LOS = sqrt(1/N/M) * ULA(angle2,N)*ULA(angle1,M)';
-G_NLOS = sqrt(1/2/M)*(randn(N,M)+1j*randn(N,M));
+G_NLOS = sqrt(1/2/M/N)*(randn(N,M)+1j*randn(N,M));
 G = (sqrt(kappa/(kappa+1))*G_LOS+sqrt(1/(kappa+1))*G_NLOS) * diag(G_pl); % BS->RIS
 
 
